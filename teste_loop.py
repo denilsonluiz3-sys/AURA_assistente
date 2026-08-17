bd23388701ea587c84daf7a349d3e11221e34624
denilsonluiz3-sys/AURA_assistente <denilsonluiz3@gmail.com>
2026-08-17 03:11:55 -0300

    Delete teste_loop.py

--- teste_loop.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print("AURA: processo iniciado", flush=True)
-
-for i in range(10):
-    print(f"AURA: execução {i + 1}/10", flush=True)
-    time.sleep(5)
-
-print("AURA: processo finalizado", flush=True)
