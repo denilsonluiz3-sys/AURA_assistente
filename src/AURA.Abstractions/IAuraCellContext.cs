dd9321d751a3a6c4475104a30e16a0a6f8c0cef9
denilsonluiz3-sys/AURA_assistente <denilsonluiz3@gmail.com>
2026-08-21 19:22:33 -0300

    feat: add cell program context contract

---
+++ src/AURA.Abstractions/IAuraCellContext.cs
@@ -0,0 +1,10 @@
+using System.Threading;
+
+namespace AURA.Abstractions;
+
+public interface IAuraCellContext
+{
+    string CellId { get; }
+    CancellationToken CancellationToken { get; }
+    IAndroidCapabilityService Android { get; }
+}
