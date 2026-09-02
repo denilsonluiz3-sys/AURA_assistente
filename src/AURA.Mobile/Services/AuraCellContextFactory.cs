709db11b80437652db8f81d47515e14f1d1954af
denilsonluiz3-sys/AURA_assistente <denilsonluiz3@gmail.com>
2026-09-02 00:54:27 -0300

    feat(browser): inject live BrowserPage into cell context

--- src/AURA.Mobile/Services/AuraCellContextFactory.cs
+++ src/AURA.Mobile/Services/AuraCellContextFactory.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Threading;
 using AURA.Abstractions;
+using AURA.Mobile.Pages;
 
 namespace AURA.Mobile.Services;
 
 public sealed class AuraCellContextFactory : IAuraCellContextFactory
 {
     private readonly IAndroidCapabilityService _android;
+    private readonly BrowserPage _browserPage;
 
-    public AuraCellContextFactory(IAndroidCapabilityService android)
+    public AuraCellContextFactory(IAndroidCapabilityService android, BrowserPage browserPage)
     {
         _android = android ?? throw new System.ArgumentNullException(nameof(android));
+        _browserPage = browserPage ?? throw new System.ArgumentNullException(nameof(browserPage));
     }
 
     public IAuraCellContext Create(string cellId, CancellationToken ct = default, IReadOnlyDictionary<string, string>? arguments = null)
-        => new AuraCellContext(cellId, _android, ct, arguments);
+        => new AuraCellContext(cellId, _android, _browserPage, ct, arguments);
 }
 #endif
